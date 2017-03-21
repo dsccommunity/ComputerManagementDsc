3f4094a5bd14a8e5810d58125eeadc457c80fe91
Jan-Hendrik Peters <nyanhp@users.noreply.github.com>
2017-03-21 15:38:53 +0100

    Added a couple of Write-Verbose statements

--- DSCResources/MSFT_xScheduledTask/MSFT_xScheduledTask.psm1
+++ DSCResources/MSFT_xScheduledTask/MSFT_xScheduledTask.psm1
@@ -14,252 +14,7 @@ namespace xScheduledTask
 }
 '@
 
-function Remove-CommonParameter
-{
-    [OutputType([hashtable])]
-    [cmdletbinding()]
-    param
-    (
-        [Parameter(Mandatory)]
-        [hashtable]
-        $Hashtable
-    )
-
-    $inputClone = $Hashtable.Clone()
-    $commonParameters = [System.Management.Automation.PSCmdlet]::CommonParameters
-    $commonParameters += [System.Management.Automation.PSCmdlet]::OptionalCommonParameters
-
-    $Hashtable.Keys | Where-Object { $_ -in $commonParameters } | ForEach-Object {
-        $inputClone.Remove($_)
-    }
-
-    $inputClone
-}
-
-function Test-DscParameterState
-{
-    [CmdletBinding()]
-    param
-    (
-        [Parameter(Mandatory)] 
-        [hashtable]
-        $CurrentValues,
-
-        [Parameter(Mandatory)] 
-        [object]
-        $DesiredValues,
-        
-        [string[]]
-        $ValuesToCheck,
-        
-        [switch]$TurnOffTypeChecking
-    )
-
-    $returnValue = $true
-
-    $types = 'System.Management.Automation.PSBoundParametersDictionary', 'System.Collections.Hashtable', 'Microsoft.Management.Infrastructure.CimInstance'
-    
-    if ($DesiredValues.GetType().FullName -notin $types)
-    {
-        throw ("Property 'DesiredValues' in Test-DscParameterState must be either a Hashtable or CimInstance. Type detected was $($DesiredValues.GetType().Name)")
-    }
-
-    if ($DesiredValues.GetType().FullName -eq 'Microsoft.Management.Infrastructure.CimInstance' -and -not $ValuesToCheck)
-    {
-        throw ("If 'DesiredValues' is a CimInstance then property 'ValuesToCheck' must contain a value")
-    }
-    
-    $DesiredValuesClean = Remove-CommonParameter -Hashtable $DesiredValues
-
-    if (-not $ValuesToCheck)
-    {
-        $keyList = $DesiredValuesClean.Keys
-    } 
-    else
-    {
-        $keyList = $ValuesToCheck
-    }
-
-    foreach ($key in $keyList)
-    {
-        if ($null -ne $DesiredValuesClean.$key)
-        {
-            $desiredType = $DesiredValuesClean.$key.GetType()
-        }
-        else
-        {
-            $desiredType = [psobject]@{ Name = 'Unknown' }
-        }
-        
-        if ($null -ne $CurrentValues.$key)
-        {
-            $currentType = $CurrentValues.$key.GetType()
-        }
-        else
-        {
-            $currentType = [psobject]@{ Name = 'Unknown' }
-        }
-
-        if ($currentType.Name -ne 'Unknown' -and $desiredType.Name -eq 'PSCredential')
-        {
-            # This is a credential object. Compare only the user name
-            if ($currentType.Name -eq 'PSCredential' -and $CurrentValues.$key.UserName -eq $DesiredValuesClean.$key.UserName)
-            {
-                Write-Verbose -Message ('MATCH: PSCredential username match. Current state is {0} and desired state is {1}' -f $CurrentValues.$key.UserName, $DesiredValuesClean.$key.UserName)
-                continue
-            }
-            else
-            {
-                Write-Verbose -Message ('NOTMATCH: PSCredential username mismatch. Current state is {0} and desired state is {1}' -f $CurrentValues.$key.UserName, $DesiredValuesClean.$key.UserName)
-                $returnValue = $false
-            }
-            
-            # Assume the string is our username when the matching desired value is actually a credential
-            if($currentType.Name -eq 'string' -and $CurrentValues.$key -eq $DesiredValuesClean.$key.UserName)
-            {
-                Write-Verbose -Message ('MATCH: PSCredential username match. Current state is {0} and desired state is {1}' -f $CurrentValues.$key, $DesiredValuesClean.$key.UserName)
-                continue
-            }
-            else
-            {
-                Write-Verbose -Message ('NOTMATCH: PSCredential username mismatch. Current state is {0} and desired state is {1}' -f $CurrentValues.$key, $DesiredValuesClean.$key.UserName)
-                $returnValue = $false
-            }
-        }
-     
-        if (-not $TurnOffTypeChecking)
-        {   
-            if (($desiredType.Name -ne 'Unknown' -and $currentType.Name -ne 'Unknown') -and
-            $desiredType.FullName -ne $currentType.FullName)
-            {
-                Write-Verbose -Message "NOTMATCH: Type mismatch for property '$key' Current state type is '$($currentType.Name)' and desired type is '$($desiredType.Name)'"
-                continue
-            }
-        }
-
-        if ($CurrentValues.$key -eq $DesiredValuesClean.$key -and -not $desiredType.IsArray)
-        {
-            Write-Verbose -Message "MATCH: Value (type $($desiredType.Name)) for property '$key' does match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValuesClean.$key)'"
-            continue
-        }
-                    
-        if ($DesiredValuesClean.GetType().Name -in 'HashTable', 'PSBoundParametersDictionary')
-        {
-            $checkDesiredValue = $DesiredValuesClean.ContainsKey($key)
-        } 
-        else
-        {
-            $checkDesiredValue = Test-DSCObjectHasProperty -Object $DesiredValuesClean -PropertyName $key
-        }
-        
-        if (-not $checkDesiredValue)
-        {
-            Write-Verbose -Message "MATCH: Value (type $($desiredType.Name)) for property '$key' does match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValuesClean.$key)'"
-            continue
-        }
-        
-        if ($desiredType.IsArray)
-        {
-            Write-Verbose "Comparing values in property '$key'"
-            if (-not $CurrentValues.ContainsKey($key) -or -not $CurrentValues.$key)
-            {
-                Write-Verbose -Message "NOTMATCH: Value (type $($desiredType.Name)) for property '$key' does not match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValuesClean.$key)'"
-                $returnValue = $false
-                continue
-            }
-            elseif ($CurrentValues.$key.Count -ne $DesiredValues.$key.Count)
-            {
-                Write-Verbose -Message "NOTMATCH: Value (type $($desiredType.Name)) for property '$key' does have a different count. Current state count is '$($CurrentValues.$key.Count)' and desired state count is '$($DesiredValuesClean.$key.Count)'"
-                $returnValue = $false
-                continue
-            }
-            else
-            {
-                $desiredArrayValues = $DesiredValues.$key
-                $currentArrayValues = $CurrentValues.$key
-
-                for ($i = 0; $i -lt $desiredArrayValues.Count; $i++)
-                {
-                    if ($null -ne $desiredArrayValues[$i])
-                    {
-                        $desiredType = $desiredArrayValues[$i].GetType()
-                    }
-                    else
-                    {
-                        $desiredType = [psobject]@{ Name = 'Unknown' }
-                    }
-                    
-                    if ($null -ne $currentArrayValues[$i])
-                    {
-                        $currentType = $currentArrayValues[$i].GetType()
-                    }
-                    else
-                    {
-                        $currentType = [psobject]@{ Name = 'Unknown' }
-                    }
-                    
-                    if (-not $TurnOffTypeChecking)
-                    {
-                        if (($desiredType.Name -ne 'Unknown' -and $currentType.Name -ne 'Unknown') -and
-                        $desiredType.FullName -ne $currentType.FullName)
-                        {
-                            Write-Verbose -Message "`tNOTMATCH: Type mismatch for property '$key' Current state type of element [$i] is '$($currentType.Name)' and desired type is '$($desiredType.Name)'"
-                            $returnValue = $false
-                            continue
-                        }
-                    }
-                        
-                    if ($desiredArrayValues[$i] -ne $currentArrayValues[$i])
-                    {
-                        Write-Verbose -Message "`tNOTMATCH: Value [$i] (type $($desiredType.Name)) for property '$key' does match. Current state is '$($currentArrayValues[$i])' and desired state is '$($desiredArrayValues[$i])'"
-                        $returnValue = $false
-                        continue
-                    }
-                    else
-                    {
-                        Write-Verbose -Message "`tMATCH: Value [$i] (type $($desiredType.Name)) for property '$key' does match. Current state is '$($currentArrayValues[$i])' and desired state is '$($desiredArrayValues[$i])'"
-                        continue
-                    }
-                }
-                
-            }
-        } 
-        else {
-            if ($DesiredValuesClean.$key -ne $CurrentValues.$key)
-            {
-                Write-Verbose -Message "NOTMATCH: Value (type $($desiredType.Name)) for property '$key' does not match. Current state is '$($CurrentValues.$key)' and desired state is '$($DesiredValuesClean.$key)'"
-                $returnValue = $false
-            }
-        
-        } 
-    }
-    
-    Write-Verbose "Result is '$returnValue'"
-    return $returnValue
-}
-
-function Test-DSCObjectHasProperty
-{
-    [CmdletBinding()]
-    [OutputType([bool])]
-    param
-    (
-        [Parameter(Mandatory)] 
-        [object]
-        $Object,
-
-        [Parameter(Mandatory)]
-        [string]
-        $PropertyName
-    )
-
-    if ($Object.PSObject.Properties.Name -contains $PropertyName) 
-    {
-        return [bool]$Object.$PropertyName
-    }
-    
-    return $false
-}
+Import-Module -Name (Join-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -ChildPath 'CommonResourceHelper.psm1')
 
 function Get-TargetResource
 {
@@ -391,10 +146,12 @@ function Get-TargetResource
         $RunOnlyIfNetworkAvailable = $false
     )
 
+    Write-Verbose -Message ('Retrieving existing task ({0} in {1})' -f $TaskName, $TaskPath)
     $task = Get-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath -ErrorAction SilentlyContinue
     
     if ($null -eq $task) 
     {
+        Write-Verbose -Message ('No task found. returning empty task {0} with Ensure = "Absent"' -f $Taskname)
         return @{
             TaskName = $TaskName
             ActionExecutable = $ActionExecutable
@@ -404,6 +161,7 @@ function Get-TargetResource
     } 
     else 
     {
+        Write-Verbose -Message ('Task {0} found in {1}. Retrieving settings, first action, first trigger and repetition settings' -f $TaskName, $TaskPath)
         $action = $task.Actions | Select-Object -First 1
         $trigger = $task.Triggers | Select-Object -First 1
         $settings = $task.Settings
@@ -446,8 +204,12 @@ function Get-TargetResource
             {
                 throw "Trigger type $_ not recognized."
             }            
-        }        
+        }
         
+        Write-Verbose -Message ('Detected schedule type {0} for first trigger' -f $returnScheduleType)
+        
+        Write-Verbose -Message 'Calculating timespans/datetimes from trigger repetition settings'
+
         $repInterval = $trigger.Repetition.Interval
         $Days = $Hours = $Minutes = $Seconds = 0
 
@@ -796,6 +558,7 @@ function Set-TargetResource
         $RunOnlyIfNetworkAvailable = $false
     )
     
+    Write-Verbose -Message ('Entering Set-TargetResource for {0} in {1}' -f $TaskName,$TaskPath)
     $currentValues = Get-TargetResource @PSBoundParameters
     
     if ($Ensure -eq "Present") 
@@ -953,19 +716,17 @@ function Set-TargetResource
             }
 
             $tempTrigger = New-ScheduledTaskTrigger -Once -At 6:6:6 -RepetitionInterval $RepeatInterval.TimeOfDay -RepetitionDuration $RepetitionDuration.TimeOfDay
-
-            Write-Verbose 'Copying values from temporary trigger to property Repetition of $trigger.Repetition'
-
+            Write-Verbose -Message 'Copying values from temporary trigger to property Repetition of $trigger.Repetition'
             $trigger.Repetition = $tempTrigger.Repetition
         }
 
         if ($currentValues.Ensure -eq "Present") 
         {
-            Write-Verbose -Message "Removing previous scheduled task `"$TaskName`""
+            Write-Verbose -Message ('Removing previous scheduled task' -f $TaskName)
             Unregister-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath
         }
         
-        Write-Verbose -Message "Creating new scheduled task `"$TaskName`""
+        Write-Verbose -Message ('Creating new scheduled task' -f $TaskName)
 
         $scheduledTask = New-ScheduledTask -Action $action -Trigger $trigger -Settings $setting
         if (-not [string]::IsNullOrWhiteSpace($Description))
@@ -994,7 +755,7 @@ function Set-TargetResource
     
     if ($Ensure -eq "Absent") 
     {
-        Write-Verbose -Message "Removing scheduled task `"$TaskName`""
+        Write-Verbose -Message ('Removing scheduled task' -f $TaskName)
         Unregister-ScheduledTask -TaskName $TaskName -TaskPath $TaskPath -Confirm:$false
     }
 }
@@ -1129,7 +890,7 @@ function Test-TargetResource
         $RunOnlyIfNetworkAvailable = $false
     )
     
-    Write-Verbose -Message "Testing scheduled task $TaskName"
+    Write-Verbose -Message ('Testing scheduled task' -f $TaskName)
 
     $CurrentValues = Get-TargetResource @PSBoundParameters
 
