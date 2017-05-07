2365c7c368696736458fda4a9aa2fa13973cfd3f
nyanhp <nyanhp@users.noreply.github.com>
2017-05-07 18:02:28 +0200

    Added new functions to throw exceptions

--- DSCResources/CommonResourceHelper.psm1
+++ DSCResources/CommonResourceHelper.psm1
@@ -211,7 +211,7 @@ function Test-DscParameterState
         
         if ($desiredType.IsArray)
         {
-            Write-Verbose "Comparing values in property '$key'"
+            Write-Verbose -Message "Comparing values in property '$key'"
             if (-not $CurrentValues.ContainsKey($key) -or -not $CurrentValues.$key)
             {
                 Write-Verbose -Message "NOTMATCH: Value (type $($desiredType.Name)) for property '$key' does not match. Current state is '$($CurrentValues.$key)' and desired state is '$($desiredValuesClean.$key)'"
@@ -290,7 +290,7 @@ function Test-DscParameterState
         } 
     }
     
-    Write-Verbose "Result is '$returnValue'"
+    Write-Verbose -Message "Result is '$returnValue'"
     return $returnValue
 }
 
@@ -325,9 +325,97 @@ function Test-DSCObjectHasProperty
     return $false
 }
 
+<#
+    .SYNOPSIS
+        Creates and throws an invalid argument exception
+
+    .PARAMETER Message
+        The message explaining why this error is being thrown
+
+    .PARAMETER ArgumentName
+        The name of the invalid argument that is causing this error to be thrown
+#>
+function New-InvalidArgumentException
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Message,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $ArgumentName
+    )
+
+    $argumentException = New-Object -TypeName 'ArgumentException' `
+                                    -ArgumentList @($Message, $ArgumentName)
+    $newObjectParams = @{
+        TypeName = 'System.Management.Automation.ErrorRecord'
+        ArgumentList = @($argumentException, $ArgumentName, 'InvalidArgument', $null)
+    }
+    $errorRecord = New-Object @newObjectParams
+
+    throw $errorRecord
+}
+
+<#
+    .SYNOPSIS
+        Creates and throws an invalid operation exception
+
+    .PARAMETER Message
+        The message explaining why this error is being thrown
+
+    .PARAMETER ErrorRecord
+        The error record containing the exception that is causing this terminating error
+#>
+function New-InvalidOperationException
+{
+    [CmdletBinding()]
+    param
+    (
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Message,
+
+        [ValidateNotNull()]
+        [System.Management.Automation.ErrorRecord]
+        $ErrorRecord
+    )
+
+    if ($null -eq $Message)
+    {
+        $invalidOperationException = New-Object -TypeName 'InvalidOperationException'
+    }
+    elseif ($null -eq $ErrorRecord)
+    {
+        $invalidOperationException = New-Object -TypeName 'InvalidOperationException' `
+                                                -ArgumentList @($Message)
+    }
+    else
+    {
+        $invalidOperationException = New-Object -TypeName 'InvalidOperationException' `
+                                                -ArgumentList @($Message, $ErrorRecord.Exception)
+    }
+
+    $newObjectParams = @{
+        TypeName = 'System.Management.Automation.ErrorRecord'
+        ArgumentList = @( $invalidOperationException.ToString(), 'MachineStateIncorrect',
+                          'InvalidOperation', $null )
+    }
+
+    $errorRecordToThrow = New-Object @newObjectParams
+    throw $errorRecordToThrow
+}
+
 Export-ModuleMember -Function @(
     'Get-LocalizedData'
     'Remove-CommonParameter'
     'Test-DscParameterState'
     'Test-DSCObjectHasProperty'
+    'New-InvalidOperationException'
+    'New-InvalidArgumentException'
 )
