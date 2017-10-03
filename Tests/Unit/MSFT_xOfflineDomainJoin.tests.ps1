cd00e922935d5f539a41bc010f29daa9ab96981c
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-10-03 21:34:50 +1300

    Minor clean up of xOfflineDomainJoin tests

--- Tests/Unit/MSFT_xOfflineDomainJoin.tests.ps1
+++ Tests/Unit/MSFT_xOfflineDomainJoin.tests.ps1
@@ -1,14 +1,14 @@
-$script:DSCModuleName      = 'xComputerManagement'
-$script:DSCResourceName    = 'MSFT_xOfflineDomainJoin'
+$script:DSCModuleName = 'xComputerManagement'
+$script:DSCResourceName = 'MSFT_xOfflineDomainJoin'
 
 Import-Module -Name (Join-Path -Path (Join-Path -Path (Split-Path $PSScriptRoot -Parent) -ChildPath 'TestHelpers') -ChildPath 'CommonTestHelper.psm1') -Global
 
 # Unit Test Template Version: 1.2.0
 $script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
 if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
+    (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    & git @('clone','https://github.com/PowerShell/DscResource.Tests.git',(Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests\'))
+    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests\'))
 }
 
 Import-Module (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1') -Force
@@ -28,7 +28,8 @@ try
 
         $TestOfflineDomainJoin = @{
             IsSingleInstance = 'Yes'
-            RequestFile = 'C:\ODJRequest.txt'
+            RequestFile      = 'C:\ODJRequest.txt'
+            Verbose          = $True
         }
 
         Describe "$($script:DSCResourceName)\Get-TargetResource" {
