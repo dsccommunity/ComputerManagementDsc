dc46cff04eb190c0c69fb1bfc923450fdb0bb53e
Outek <daniel.siegenthaler.1@gmail.com>
2018-05-21 11:02:28 +0200

    Replace Header in integration test

--- Tests/Integration/MSFT_PowerShellExecutionPolicy.Integration.Tests.ps1
+++ Tests/Integration/MSFT_PowerShellExecutionPolicy.Integration.Tests.ps1
@@ -2,12 +2,12 @@ $script:DSCModuleName = 'ComputerManagementDsc'
 $script:DSCResourceName = 'MSFT_PowerShellExecutionPolicy'
 
 #region HEADER
-# Integration Test Template Version: 1.2.0
-[String] $script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
+# Integration Test Template Version: 1.1.1
+$script:moduleRoot = Join-Path -Path $(Split-Path -Parent (Split-Path -Parent (Split-Path -Parent $Script:MyInvocation.MyCommand.Path))) -ChildPath 'Modules\ComputerManagementDsc'
 if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
     (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
 {
-    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))
+    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $script:moduleRoot -ChildPath '\DSCResource.Tests\'))
 }
 
 Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (Join-Path -Path 'DSCResource.Tests' -ChildPath 'TestHelper.psm1')) -Force
