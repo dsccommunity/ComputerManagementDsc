d4db796856e119d67cc882f35f1af17d796f4fa0
Daniel Siegenthaler <daniel.siegenthaler.1@gmail.com>
2018-09-09 13:31:00 +0200

    Example

--- Modules/ComputerManagementDsc/Examples/Resources/WinEventlog/1-SetWinEventlogSize.ps1
+++ Modules/ComputerManagementDsc/Examples/Resources/WinEventlog/1-SetWinEventlogSize.ps1
@@ -11,38 +11,10 @@ Configuration Example
     {
         WinEventLog ApplicationEventlog
         {
-            LogName            = "Application"
+            LogName            = 'Application'
             IsEnabled          = $true
-            LogMode            = "Circular"
+            LogMode            = 'Circular'
             MaximumSizeInBytes = 4096mb
         } # End of WinEventLog Resource
     } # End of Node
 } # End of Configuration
-
-Example -OutputPath C:\Temp
-
-Start-DscConfiguration -Path C:\Temp\localhost.mof -Verbose
-
-
-$before = Get-WinEvent -ListLog "Microsoft-Windows-MSPaint/Admin"
-configuration Demo1
-{
-    Import-DscResource -module ComputerManagementDsc
-
-    WinEventLog Demo1
-    {
-        LogName            = "Microsoft-Windows-MSPaint/Admin"
-        IsEnabled          = $true
-        LogMode            = "AutoBackup"
-        MaximumSizeInBytes = 20mb
-    }
-}
-
-Demo1 -OutputPath C:\Temp
-
-Start-DscConfiguration -Path C:\Temp -ComputerName localhost -Verbose -wait -debug
-
-
-$after = Get-WinEvent -ListLog "Microsoft-Windows-MSPaint/Admin"
-$before,$after | format-table -AutoSize LogName,IsEnabled,MaximumSizeInBytes,ProviderLatency,LogMode
-Get-DscConfiguration
