d7c9751a7589a5e149d31097770ebafeb53b2025
Daniel Siegenthaler <daniel.siegenthaler.1@gmail.com>
2018-09-14 21:43:35 +0200

    One more

--- Tests/Integration/MSFT_WinEventLog.config.ps1
+++ Tests/Integration/MSFT_WinEventLog.config.ps1
@@ -1,4 +1,21 @@
 # Integration Test Config Template Version: 1.0.0
+configuration MSFT_WinEventLog_Default
+{
+    Import-DscResource -ModuleName ComputerManagementDsc
+    node 'localhost'
+    {
+        WinEventLog Integration_Test
+        {
+            LogName            = 'Application'
+            IsEnabled          = $true
+            LogMode            = 'Circular'
+            MaximumSizeInBytes = '20971520'
+            LogFilePath        = '%SystemRoot%\System32\Winevt\Logs\Application.evtx'
+            SecurityDescriptor = 'O:BAG:SYD:(D;; 0xf0007;;;AN)(D;; 0xf0007;;;BG)(A;; 0xf0007;;;SY)(A;; 0x5;;;BA)(A;; 0x7;;;SO)(A;; 0x3;;;IU)(A;; 0x2;;;BA)(A;; 0x2;;;LS)(A;; 0x2;;;NS)'
+        }
+    }
+}
+
 configuration MSFT_WinEventLog_RetainSize
 {
     Import-DscResource -ModuleName ComputerManagementDsc
@@ -57,23 +74,6 @@ configuration MSFT_WinEventLog_CircularSecurityDescriptor
             LogMode            = 'Circular'
             MaximumSizeInBytes = '20971520'
             SecurityDescriptor = 'O:BAG:SYD:(D;; 0xf0007;;;AN)(A;; 0x7;;;SO)(A;; 0x3;;;IU)'
-        }
-    }
-}
-
-configuration MSFT_WinEventLog_Default
-{
-    Import-DscResource -ModuleName ComputerManagementDsc
-    node 'localhost'
-    {
-        WinEventLog Integration_Test
-        {
-            LogName            = 'Application'
-            IsEnabled          = $true
-            LogMode            = 'Circular'
-            MaximumSizeInBytes = '20971520'
-            LogFilePath        = '%SystemRoot%\System32\Winevt\Logs\Application.evtx'
-            SecurityDescriptor = 'O:BAG:SYD:(D;; 0xf0007;;;AN)(D;; 0xf0007;;;BG)(A;; 0xf0007;;;SY)(A;; 0x5;;;BA)(A;; 0x7;;;SO)(A;; 0x3;;;IU)(A;; 0x2;;;BA)(A;; 0x2;;;LS)(A;; 0x2;;;NS)'
         }
     }
 }
