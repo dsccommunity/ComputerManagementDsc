6df64ef318fcd7674cc07e35b334f37309555e3e
Mark Lewis <mark.lewis@zatzy.co.uk>
2019-09-11 15:27:06 +0100

    Fixing failed test by readding the parameters that are required

--- Tests/Unit/MSFT_SmbServerConfiguration.Tests.ps1
+++ Tests/Unit/MSFT_SmbServerConfiguration.Tests.ps1
@@ -252,6 +252,51 @@ try
                 ValidateTargetName              = $true
             }
         }
+        $FullParams = @{
+            IsSingleInstance                = 'Yes'
+            AnnounceComment                 = ''
+            AnnounceServer                  = $false
+            AsynchronousCredits             = 64
+            AuditSmb1Access                 = $false
+            AutoDisconnectTimeout           = 15
+            AutoShareServer                 = $true
+            AutoShareWorkstation            = $true
+            CachedOpenLimit                 = 10
+            DurableHandleV2TimeoutInSeconds = 180
+            EnableAuthenticateUserSharing   = $false
+            EnableDownlevelTimewarp         = $false
+            EnableForcedLogoff              = $true
+            EnableLeasing                   = $true
+            EnableMultiChannel              = $true
+            EnableOplocks                   = $true
+            EnableSecuritySignature         = $false
+            EnableSMB1Protocol              = $false
+            EnableSMB2Protocol              = $true
+            EnableStrictNameChecking        = $true
+            EncryptData                     = $false
+            IrpStackSize                    = 15
+            KeepAliveTime                   = 2
+            MaxChannelPerSession            = 32
+            MaxMpxCount                     = 50
+            MaxSessionPerConnection         = 16384
+            MaxThreadsPerQueue              = 20
+            MaxWorkItems                    = 1
+            NullSessionPipes                = ''
+            NullSessionShares               = ''
+            OplockBreakWait                 = 35
+            PendingClientTimeoutInSeconds   = 120
+            RejectUnencryptedAccess         = $true
+            RequireSecuritySignature        = $false
+            ServerHidden                    = $true
+            Smb2CreditsMax                  = 2048
+            Smb2CreditsMin                  = 128
+            SmbServerNameHardeningLevel     = 0
+            TreatHostAsStableStorage        = $false
+            ValidateAliasNotCircular        = $true
+            ValidateShareScope              = $true
+            ValidateShareScopeNotAliased    = $true
+            ValidateTargetName              = $true
+        }
 
         Describe 'MSFT_SmbServerConfiguration\Get-TargetResource' -Tag 'Get' {
             Context 'When getting the Target Resource information' {
@@ -259,8 +304,48 @@ try
 
                     $SmbServerConfiguration = Get-TargetResource -IsSingleInstance Yes
 
-                    $SmbServerConfiguration.AuditSmb1Access | Should -Not -BeNullOrEmpty
                     $SmbServerConfiguration.EnableSMB1Protocol | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AnnounceComment | Should -BeOfType String
+                    $SmbServerConfiguration.AnnounceServer | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AsynchronousCredits | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AuditSmb1Access | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AutoDisconnectTimeout | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AutoShareServer | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.AutoShareWorkstation | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.CachedOpenLimit | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.DurableHandleV2TimeoutInSeconds | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableAuthenticateUserSharing | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableDownlevelTimewarp | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableForcedLogoff | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableLeasing | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableMultiChannel | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableOplocks | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableSecuritySignature | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableSMB2Protocol | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EnableStrictNameChecking | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.EncryptData | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.IrpStackSize | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.KeepAliveTime | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.MaxChannelPerSession | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.MaxMpxCount | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.MaxSessionPerConnection | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.MaxThreadsPerQueue | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.MaxWorkItems | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.NullSessionPipes | Should -BeOfType String
+                    $SmbServerConfiguration.NullSessionShares | Should -BeOfType String
+                    $SmbServerConfiguration.OplockBreakWait | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.PendingClientTimeoutInSeconds | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.RejectUnencryptedAccess | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.RequireSecuritySignature | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.ServerHidden | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.Smb2CreditsMax | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.Smb2CreditsMin | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.SmbServerNameHardeningLevel | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.TreatHostAsStableStorage | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.ValidateAliasNotCircular | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.ValidateShareScope | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.ValidateShareScopeNotAliased | Should -Not -BeNullOrEmpty
+                    $SmbServerConfiguration.ValidateTargetName| Should -Not -BeNullOrEmpty
                 }
             }
         }
@@ -270,7 +355,7 @@ try
                 It 'Test-TargetResource should return false' {
                     Mock -CommandName Get-SmbServerConfiguration { return $mocks.DefaultSettings }
 
-                    $TestEnvironmentResult = Test-TargetResource @Params
+                    $TestEnvironmentResult = Test-TargetResource @FullParams
                     $TestEnvironmentResult | Should -BeFalse
                 }
             }
