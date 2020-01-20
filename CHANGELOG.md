5609c724ffdb6a13b4be08335cfe53ceb3dff09d
Daniel Scott-Raynsford <plagueho@gmail.com>
2020-01-20 20:55:50 +1300

    Fix first integration test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# Change log for NetworkingDSc
+# Change log for ComputerManagementDsc
 
 The format is based on and uses the types of changes according to [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
--- tests/Integration/DSC_PendingReboot.Tests.ps1
+++ tests/Integration/DSC_PendingReboot.Tests.ps1
@@ -1,115 +1,118 @@
-#region HEADER
-$script:dscModuleName = 'ComputerManagementDsc'
+$script:dscModuleName = 'NetworkingDsc'
 $script:dscResourceName = 'DSC_PendingReboot'
-
-# Integration Test Template Version: 1.3.3
-$script:moduleRoot = Split-Path -Parent (Split-Path -Parent $PSScriptRoot)
-if ( (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests'))) -or `
-    (-not (Test-Path -Path (Join-Path -Path $script:moduleRoot -ChildPath 'DSCResource.Tests\TestHelper.psm1'))) )
-{
-    & git @('clone', 'https://github.com/PowerShell/DscResource.Tests.git', (Join-Path -Path $script:moduleRoot -ChildPath 'DscResource.Tests'))
-}
-
-Import-Module -Name (Join-Path -Path $script:moduleRoot -ChildPath (Join-Path -Path 'DSCResource.Tests' -ChildPath 'TestHelper.psm1')) -Force
-$TestEnvironment = Initialize-TestEnvironment `
-    -DSCModuleName $script:dscModuleName `
-    -DSCResourceName $script:dscResourceName `
-    -TestType Integration
-#endregion
 
 try
 {
-    $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
-    . $configFile
+    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+}
+catch [System.IO.FileNotFoundException]
+{
+    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+}
 
-    Describe "$($script:dscResourceName)_Integration" {
-        <#
-            These integration tests will not actually reboot the node
-            because that would terminate the tests and cause them to fail.
+$script:testEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $script:dscModuleName `
+    -DSCResourceName $script:dscResourceName `
+    -ResourceType 'Mof' `
+    -TestType 'Integration'
 
-            There does not appear to be a method of determining if the
-            reboot is in fact triggered, so this is not currently tested.
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
 
-            Instead, we will preserve the current state of the Auto Update
-            reboot flag and then set it to reboot required. After the tests
-            have run we will determine if the Get-TargetResource indicates
-            that a reboot would have been required.
-        #>
-        $windowsUpdateKeys = (Get-ChildItem -Path $rebootRegistryKeys.WindowsUpdate).Name
+# Begin Testing
+try
+{
+    Describe 'PendingReboot Integration Tests' {
+        $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+        . $configFile
 
-        if ($windowsUpdateKeys)
-        {
-            $script:currentAutoUpdateRebootState = $windowsUpdateKeys.Split('\') -contains 'RebootRequired'
-        }
+        Describe "$($script:dscResourceName)_Integration" {
+            <#
+                These integration tests will not actually reboot the node
+                because that would terminate the tests and cause them to fail.
 
-        if (-not $script:currentAutoUpdateRebootState)
-        {
-            $null = New-Item `
-                -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\' `
-                -Name 'RebootRequired'
-        }
+                There does not appear to be a method of determining if the
+                reboot is in fact triggered, so this is not currently tested.
 
-        $configData = @{
-            AllNodes = @(
-                @{
-                    NodeName   = 'localhost'
-                    RebootName = 'TestReboot'
-                    SkipComponentBasedServicing = $false
-                    SkipWindowsUpdate           = $false
-                    SkipPendingFileRename       = $false
-                    SkipPendingComputerRename   = $false
-                    SkipCcmClientSDK            = $true
-                }
-            )
-        }
+                Instead, we will preserve the current state of the Auto Update
+                reboot flag and then set it to reboot required. After the tests
+                have run we will determine if the Get-TargetResource indicates
+                that a reboot would have been required.
+            #>
+            $windowsUpdateKeys = (Get-ChildItem -Path $rebootRegistryKeys.WindowsUpdate).Name
 
-        It 'Should compile and apply the MOF without throwing' {
+            if ($windowsUpdateKeys)
             {
-                & "$($script:dscResourceName)_Config" `
-                    -OutputPath $TestDrive `
-                    -ConfigurationData $configData
-
-                $startDscConfigurationParameters = @{
-                    Path         = $TestDrive
-                    ComputerName = 'localhost'
-                    Wait         = $true
-                    Verbose      = $true
-                    Force        = $true
-                    ErrorAction  = 'Stop'
-                }
-
-                Start-DscConfiguration @startDscConfigurationParameters
-            } | Should -Not -Throw
-        }
-
-        It 'Should be able to call Get-DscConfiguration without throwing' {
-            {
-                Get-DscConfiguration -Verbose -ErrorAction Stop
-            } | Should -Not -Throw
-        }
-
-        It 'Should have set the resource and all the parameters should match' {
-            $current = Get-DscConfiguration | Where-Object -FilterScript {
-                $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+                $script:currentAutoUpdateRebootState = $windowsUpdateKeys.Split('\') -contains 'RebootRequired'
             }
-            $current.Name | Should -Be $configData.AllNodes[0].RebootName
-            $current.SkipComponentBasedServicing | Should -Be $configData.AllNodes[0].SkipComponentBasedServicing
-            $current.ComponentBasedServicing | Should -BeFalse
-            $current.SkipWindowsUpdate | Should -Be $configData.AllNodes[0].SkipWindowsUpdate
-            $current.WindowsUpdate | Should -BeTrue
-            $current.SkipPendingFileRename | Should -Be $configData.AllNodes[0].SkipPendingFileRename
-            $current.PendingFileRename | Should -BeFalse
-            $current.SkipPendingComputerRename | Should -Be $configData.AllNodes[0].SkipPendingComputerRename
-            $current.PendingComputerRename | Should -BeFalse
-            $current.SkipCcmClientSDK | Should -Be $configData.AllNodes[0].SkipCcmClientSDK
-            $current.CcmClientSDK | Should -BeFalse
-            $current.RebootRequired | Should -BeTrue
+
+            if (-not $script:currentAutoUpdateRebootState)
+            {
+                $null = New-Item `
+                    -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\' `
+                    -Name 'RebootRequired'
+            }
+
+            $configData = @{
+                AllNodes = @(
+                    @{
+                        NodeName   = 'localhost'
+                        RebootName = 'TestReboot'
+                        SkipComponentBasedServicing = $false
+                        SkipWindowsUpdate           = $false
+                        SkipPendingFileRename       = $false
+                        SkipPendingComputerRename   = $false
+                        SkipCcmClientSDK            = $true
+                    }
+                )
+            }
+
+            It 'Should compile and apply the MOF without throwing' {
+                {
+                    & "$($script:dscResourceName)_Config" `
+                        -OutputPath $TestDrive `
+                        -ConfigurationData $configData
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
+
+            It 'Should be able to call Get-DscConfiguration without throwing' {
+                {
+                    Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+            It 'Should have set the resource and all the parameters should match' {
+                $current = Get-DscConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq "$($script:dscResourceName)_Config"
+                }
+                $current.Name | Should -Be $configData.AllNodes[0].RebootName
+                $current.SkipComponentBasedServicing | Should -Be $configData.AllNodes[0].SkipComponentBasedServicing
+                $current.ComponentBasedServicing | Should -BeFalse
+                $current.SkipWindowsUpdate | Should -Be $configData.AllNodes[0].SkipWindowsUpdate
+                $current.WindowsUpdate | Should -BeTrue
+                $current.SkipPendingFileRename | Should -Be $configData.AllNodes[0].SkipPendingFileRename
+                $current.PendingFileRename | Should -BeFalse
+                $current.SkipPendingComputerRename | Should -Be $configData.AllNodes[0].SkipPendingComputerRename
+                $current.PendingComputerRename | Should -BeFalse
+                $current.SkipCcmClientSDK | Should -Be $configData.AllNodes[0].SkipCcmClientSDK
+                $current.CcmClientSDK | Should -BeFalse
+                $current.RebootRequired | Should -BeTrue
+            }
         }
     }
 }
 finally
 {
-    #region FOOTER
     if (-not $script:currentAutoUpdateRebootState)
     {
         $null = Remove-Item `
@@ -117,6 +120,5 @@ finally
             -ErrorAction SilentlyContinue
     }
 
-    Restore-TestEnvironment -TestEnvironment $TestEnvironment
-    #endregion
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
 }
