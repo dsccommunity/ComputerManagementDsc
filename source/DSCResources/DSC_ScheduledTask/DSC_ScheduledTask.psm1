ead94537cc88bdc83f32331a9cda6bb0fd2e8f6b
Daniel Scott-Raynsford <plagueho@gmail.com>
2020-05-03 19:17:45 +1200

    Remove debugging code

--- source/DSCResources/DSC_ScheduledTask/DSC_ScheduledTask.psm1
+++ source/DSCResources/DSC_ScheduledTask/DSC_ScheduledTask.psm1
@@ -1858,9 +1858,6 @@ function Get-CurrentResource
 
         if (($result.ContainsKey('LogonType')) -and ($result['LogonType'] -ieq 'ServiceAccount'))
         {
-            Write-Verbose -Message 'LogonType is ServiceAccount' -Verbose
-            Write-Verbose -Message ($task.Principal | Out-String) -Verbose
-
             $builtInAccount = $task.Principal.UserId
 
             if (-not $builtInAccount.Contains('\'))
