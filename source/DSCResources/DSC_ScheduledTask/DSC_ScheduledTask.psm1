05ba5bc8d9cac806e349ca7eec766f17da0f7afd
jensotto <jensotto@outlook.com>
2021-02-09 13:53:46 +0100

    Prevented an issue when the scheduled task is not yet created.

--- source/DSCResources/DSC_ScheduledTask/DSC_ScheduledTask.psm1
+++ source/DSCResources/DSC_ScheduledTask/DSC_ScheduledTask.psm1
@@ -1449,12 +1449,12 @@ function Test-TargetResource
             To resolve this, add the computer name if it is missing and if it was passed in the
             ExecuteAsCredential parameter.
         #>
-        if ($username -match "$ENV:COMPUTERNAME\\")
+        if ($username -match "$ENV:COMPUTERNAME\\" -and $currentValues.ExecuteAsCredential)
         {
             $currentValues.ExecuteAsCredential = Set-DomainNameInAccountName -AccountName $currentValues.ExecuteAsCredential -DomainName $ENV:COMPUTERNAME
         }
 
-        if ($username -match "$ENV:USERDOMAIN\\")
+        if ($username -match "$ENV:USERDOMAIN\\" -and $currentValues.ExecuteAsCredential)
         {
             $currentValues.ExecuteAsCredential = Set-DomainNameInAccountName -AccountName $currentValues.ExecuteAsCredential -DomainName $ENV:USERDOMAIN
         }
