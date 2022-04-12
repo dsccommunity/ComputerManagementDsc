87a96d6dda3abc5ddf2bed89859d4cf684fbd114
Nick Germany <nickjgermany@outlook.com>
2022-04-11 22:20:08 -0400

    use correct error func

--- tests/Unit/DSC_Computer.Tests.ps1
+++ tests/Unit/DSC_Computer.Tests.ps1
@@ -1096,7 +1096,7 @@ try
                 }
 
                 It 'Should throw if PasswordPass is present in options without UnsecuredJoin' {
-                    $errorRecord = New-InvalidArgumentException `
+                    $errorRecord = Get-InvalidArgumentException `
                         -Message ($LocalizedData.InvalidOptionPasswordPassUnsecuredJoin) `
                         -ArgumentName 'PasswordPass'
 
@@ -1119,7 +1119,7 @@ try
                 }
 
                 It 'Should throw if PasswordPass and UnsecuredJoin is present but credential username is not null' {
-                    $errorRecord = New-InvalidArgumentException `
+                    $errorRecord = Get-InvalidArgumentException `
                         -Message ($LocalizedData.InvalidOptionCredentialUnsecuredJoinNullUsername) `
                         -ArgumentName 'Credential'
 
