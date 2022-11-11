c37413ec508baeb95c1f47f248e34e9dfa2f80c6
Nick Germany <nickgw@outlook.com>
2022-11-11 18:42:33 -0500

    adding integration

---
+++ tests/Integration/Classes/PSResourceRepository.config.ps1
@@ -0,0 +1,91 @@
+$ConfigurationData = @{
+    AllNodes    = , @{
+        NodeName        = 'localhost'
+        CertificateFile = $Null
+    }
+    NonNodeData = @{
+        PSResourceRepository_Create_Config = @{
+            Name           = 'ExampleRepository'
+            Ensure         = 'Present'
+            SourceLocation = 'https://examplerepository.com/api/v2'
+        }
+        PSResourceRepository_Modify_Config = @{
+            Name                  = 'ExampleRepository'
+            Ensure                = 'Present'
+            SourceLocation        = 'https://examplerepository.com/api/v2'
+            ScriptSourceLocation  = 'https://www.notcorrect.com/api/v2/items/psscript'
+            PublishLocation       = 'https://www.notcorrect.com/api/v2/package/'
+            ScriptPublishLocation = 'https://www.notcorrect.com/api/v2/package/'
+            InstallationPolicy    = 'Trusted'
+
+        }
+        PSResourceRepository_Remove_Config = @{
+            Name           = 'ExampleRepository'
+            Ensure         = 'Absent'
+            SourceLocation = 'https://examplerepository.com/api/v2'
+        }
+
+    }
+}
+
+<#
+    .SYNOPSIS
+        Register a PSRepository
+#>
+configuration PSResourceRepository_Create_Config
+{
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
+
+    node $AllNodes.NodeName
+    {
+        PSResourceRepository "Register Repository $($ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.Name)"
+        {
+            Name           = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.Name
+            Ensure         = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.Ensure
+            SourceLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.SourceLocation
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+        Modifies an existing PSRepository
+#>
+configuration PSResourceRepository_Modify_Config
+{
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
+
+    node $AllNodes.NodeName
+    {
+        PSResourceRepository "Modify PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Name)"
+        {
+            Name                  = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.Name
+            Ensure                = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.Ensure
+            SourceLocation        = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.SourceLocation
+            ScriptSourceLocation  = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.ScriptSourceLocation
+            PublishLocation       = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.PublishLocation
+            ScriptPublishLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.ScriptPublishLocation
+            InstallationPolicy    = $ConfigurationData.NonNodeData.PSResourceRepository_Create_Config.InstallationPolicy
+        }
+    }
+}
+
+<#
+    .SYNOPSIS
+        Unregister an existing PSRepository
+#>
+configuration PSResourceRepository_Remove_Config
+{
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
+
+    node $AllNodes.NodeName
+    {
+        PSResourceRepository "Remove PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name)"
+        {
+            Name           = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name
+            Ensure         = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Ensure
+            SourceLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.SourceLocation
+        }
+    }
+}
+
---
+++ tests/Integration/Classes/PSResourceRepository.integration.tests.ps1
@@ -0,0 +1,20 @@
+$script:dscModuleName = 'ComputerManagementDsc'
+$script:dscResourceFriendlyName = 'PSResourceRepository'
+$script:dscResourceName = "$($script:dscResourceFriendlyName)"
+
+try
+{
+    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+}
+catch [System.IO.FileNotFoundException]
+{
+    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+}
+
+$initializationParams = @{
+    DSCModuleName = $script:dscModuleName
+    DSCResourceName = $script:dscResourceName
+    ResourceType = 'Class'
+    TestType = 'Integration'
+}
+$script:testEnvironment = Initialize-TestEnvironment @initializationParams
