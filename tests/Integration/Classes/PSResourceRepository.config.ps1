ff3a29ab1ce9aa7468e1fc2b131313f350c01ad2
Nick Germany <nickgw@outlook.com>
2022-11-14 15:09:29 -0500

    Use PSGallery for example

--- tests/Integration/Classes/PSResourceRepository.config.ps1
+++ tests/Integration/Classes/PSResourceRepository.config.ps1
@@ -5,24 +5,23 @@ $ConfigurationData = @{
     }
     NonNodeData = @{
         PSResourceRepository_Create_Config = @{
-            Name           = 'ExampleRepository'
+            Name           = 'PSGallery'
             Ensure         = 'Present'
-            SourceLocation = 'https://examplerepository.com/api/v2'
+            SourceLocation = 'https://www.powershellgallery.com/api/v2'
         }
         PSResourceRepository_Modify_Config = @{
-            Name                  = 'ExampleRepository'
+            Name                  = 'PSGallery'
             Ensure                = 'Present'
-            SourceLocation        = 'https://examplerepository.com/api/v2'
-            ScriptSourceLocation  = 'https://www.notcorrect.com/api/v2/items/psscript'
-            PublishLocation       = 'https://www.notcorrect.com/api/v2/package/'
-            ScriptPublishLocation = 'https://www.notcorrect.com/api/v2/package/'
+            SourceLocation        = 'https://www.powershellgallery.com/api/v2'
+            PublishLocation       = 'https://www.powershellgallery.com/api/v2/package/'
+            ScriptSourceLocation  = 'https://www.powershellgallery.com/api/v2/items/psscript'
+            ScriptPublishLocation = 'https://www.powershellgallery.com/api/v2/package/'
             InstallationPolicy    = 'Trusted'
-
         }
         PSResourceRepository_Remove_Config = @{
-            Name           = 'ExampleRepository'
+            Name           = 'PSGallery'
             Ensure         = 'Absent'
-            SourceLocation = 'https://examplerepository.com/api/v2'
+            SourceLocation = 'https://www.powershellgallery.com/api/v2'
         }
 
     }
@@ -47,45 +46,45 @@ configuration PSResourceRepository_Create_Config
     }
 }
 
-# <#
-#     .SYNOPSIS
-#         Modifies an existing PSRepository
-# #>
-# configuration PSResourceRepository_Modify_Config
-# {
-#     Import-DscResource -ModuleName 'ComputerManagementDsc'
+<#
+    .SYNOPSIS
+        Modifies an existing PSRepository
+#>
+configuration PSResourceRepository_Modify_Config
+{
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
 
-#     node $AllNodes.NodeName
-#     {
-#         PSResourceRepository "Modify PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Name)"
-#         {
-#             Name                  = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Name
-#             Ensure                = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Ensure
-#             SourceLocation        = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.SourceLocation
-#             ScriptSourceLocation  = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.ScriptSourceLocation
-#             PublishLocation       = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.PublishLocation
-#             ScriptPublishLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.ScriptPublishLocation
-#             InstallationPolicy    = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.InstallationPolicy
-#         }
-#     }
-# }
+    node $AllNodes.NodeName
+    {
+        PSResourceRepository "Modify PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Name)"
+        {
+            Name                  = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Name
+            Ensure                = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.Ensure
+            SourceLocation        = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.SourceLocation
+            ScriptSourceLocation  = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.ScriptSourceLocation
+            PublishLocation       = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.PublishLocation
+            ScriptPublishLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.ScriptPublishLocation
+            InstallationPolicy    = $ConfigurationData.NonNodeData.PSResourceRepository_Modify_Config.InstallationPolicy
+        }
+    }
+}
 
-# <#
-#     .SYNOPSIS
-#         Unregister an existing PSRepository
-# #>
-# configuration PSResourceRepository_Remove_Config
-# {
-#     Import-DscResource -ModuleName 'ComputerManagementDsc'
+<#
+    .SYNOPSIS
+        Unregister an existing PSRepository
+#>
+configuration PSResourceRepository_Remove_Config
+{
+    Import-DscResource -ModuleName 'ComputerManagementDsc'
 
-#     node $AllNodes.NodeName
-#     {
-#         PSResourceRepository "Remove PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name)"
-#         {
-#             Name           = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name
-#             Ensure         = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Ensure
-#             SourceLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.SourceLocation
-#         }
-#     }
-# }
+    node $AllNodes.NodeName
+    {
+        PSResourceRepository "Remove PSRepository $($ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name)"
+        {
+            Name           = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Name
+            Ensure         = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.Ensure
+            SourceLocation = $ConfigurationData.NonNodeData.PSResourceRepository_Remove_Config.SourceLocation
+        }
+    }
+}
 
