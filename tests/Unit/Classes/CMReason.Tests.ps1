3f8d32b42f27bde31a64b4c458a8f9412d98747f
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-09-09 20:57:15 +0100

    Update CMReason

--- tests/Unit/Classes/CMReason.Tests.ps1
+++ tests/Unit/Classes/CMReason.Tests.ps1
@@ -7,28 +7,29 @@
 [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseDeclaredVarsMoreThanAssignments', '')]
 param ()
 
-try
-{
-    if (-not (Get-Module -Name 'DscResource.Test'))
+BeforeDiscovery {
+    try
     {
-        # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
-        if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+        if (-not (Get-Module -Name 'DscResource.Test'))
         {
-            # Redirect all streams to $null, except the error stream (stream 2)
-            & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
-        }
+            # Assumes dependencies has been resolved, so if this module is not available, run 'noop' task.
+            if (-not (Get-Module -Name 'DscResource.Test' -ListAvailable))
+            {
+                # Redirect all streams to $null, except the error stream (stream 2)
+                & "$PSScriptRoot/../../build.ps1" -Tasks 'noop' 2>&1 4>&1 5>&1 6>&1 > $null
+            }
 
-        # If the dependencies has not been resolved, this will throw an error.
-        Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+            # If the dependencies has not been resolved, this will throw an error.
+            Import-Module -Name 'DscResource.Test' -Force -ErrorAction 'Stop'
+        }
+    }
+    catch [System.IO.FileNotFoundException]
+    {
+        throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
     }
 }
-catch [System.IO.FileNotFoundException]
-{
-    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -ResolveDependency -Tasks build" first.'
-}
 
-try
-{
+BeforeAll {
     $script:dscModuleName = 'ComputerManagementDsc'
 
     Import-Module -Name $script:dscModuleName
@@ -36,46 +37,58 @@ try
     $PSDefaultParameterValues['InModuleScope:ModuleName'] = $script:dscModuleName
     $PSDefaultParameterValues['Mock:ModuleName'] = $script:dscModuleName
     $PSDefaultParameterValues['Should:ModuleName'] = $script:dscModuleName
-
-    Describe 'CMReason' -Tag 'CMReason' {
-        Context 'When instantiating the class' {
-            It 'Should not throw an error' {
-                $script:mockCMReasonInstance = InModuleScope -ScriptBlock {
-                    [CMReason]::new()
-                }
-            }
-
-            It 'Should be of the correct type' {
-                $mockCMReasonInstance | Should -Not -BeNullOrEmpty
-                $mockCMReasonInstance.GetType().Name | Should -Be 'CMReason'
-            }
-        }
-
-        Context 'When setting and reading values' {
-            It 'Should be able to set value in instance' {
-                $script:mockCMReasonInstance = InModuleScope -ScriptBlock {
-                    $CMReasonInstance = [CMReason]::new()
-
-                    $CMReasonInstance.Code = 'SqlAudit:SqlAudit:Ensure'
-                    $CMReasonInstance.Phrase = 'The property Ensure should be "Present", but was "Absent"'
-
-                    return $CMReasonInstance
-                }
-            }
-
-            It 'Should be able read the values from instance' {
-                $mockCMReasonInstance.Code | Should -Be 'SqlAudit:SqlAudit:Ensure'
-                $mockCMReasonInstance.Phrase = 'The property Ensure should be "Present", but was "Absent"'
-            }
-        }
-    }
 }
-finally
-{
+
+AfterAll {
     $PSDefaultParameterValues.Remove('InModuleScope:ModuleName')
     $PSDefaultParameterValues.Remove('Mock:ModuleName')
     $PSDefaultParameterValues.Remove('Should:ModuleName')
 
     # Unload the module being tested so that it doesn't impact any other tests.
     Get-Module -Name $script:dscModuleName -All | Remove-Module -Force
+}
+
+Describe 'CMReason' -Tag 'CMReason' {
+    Context 'When instantiating the class' {
+        It 'Should not throw an error' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                { [CMReason]::new() } | Should -Not -Throw
+            }
+        }
+
+        It 'Should be of the correct type' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                $mockCMReasonInstance = [CMReason]::new()
+
+                $mockCMReasonInstance | Should -Not -BeNullOrEmpty
+                $mockCMReasonInstance.GetType().Name | Should -Be 'CMReason'
+            }
+        }
+    }
+
+    Context 'When setting and reading values' {
+        BeforeAll {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                $script:mockCMReasonInstance = [CMReason] @{
+                    Code   = 'SqlAudit:SqlAudit:Ensure'
+                    Phrase = 'The property Ensure should be "Present", but was "Absent"'
+                }
+            }
+        }
+
+        It 'Should be able read the values from instance' {
+            InModuleScope -ScriptBlock {
+                Set-StrictMode -Version 1.0
+
+                $script:mockCMReasonInstance.Code | Should -Be 'SqlAudit:SqlAudit:Ensure'
+                $script:mockCMReasonInstance.Phrase = 'The property Ensure should be "Present", but was "Absent"'
+            }
+        }
+    }
 }
