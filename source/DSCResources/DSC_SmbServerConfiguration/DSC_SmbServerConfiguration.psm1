2be08471da6649bd5e8be105ee75d73645c5e994
Daniel Hughes <daniel.hughes@hetalogic.com>
2024-09-09 22:05:33 +0100

    Removed argument not required

--- source/DSCResources/DSC_SmbServerConfiguration/DSC_SmbServerConfiguration.psm1
+++ source/DSCResources/DSC_SmbServerConfiguration/DSC_SmbServerConfiguration.psm1
@@ -35,7 +35,7 @@ function Get-TargetResource
         $IsSingleInstance
     )
 
-    Write-Verbose -Message ($script:localizedData.GetTargetResourceMessage -f $Name)
+    Write-Verbose -Message ($script:localizedData.GetTargetResourceMessage)
 
     $smbReturn = @{}
     $smbServer = Get-SmbServerConfiguration -ErrorAction 'SilentlyContinue'
