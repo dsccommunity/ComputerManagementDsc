324904c2ebc76d31aab869c650713c42c2c4e830
Chris Hill <chris.hill@mafint.org>
2024-11-26 17:05:04 +0000

    Restore startTimeStringWithOffset

--- tests/Unit/DSC_ScheduledTask.Tests.ps1
+++ tests/Unit/DSC_ScheduledTask.Tests.ps1
@@ -2664,6 +2664,10 @@ Describe 'DSC_ScheduledTask' {
     }
 
     Context 'When a scheduled task is created and synchronize across time zone is enabled' {
+        BeforeDiscovery {
+            $startTimeStringWithOffset = '2018-10-01T01:00:00-08:00'
+        }
+
         BeforeAll {
             $startTimeString = '2018-10-01T01:00:00'
             $startTimeStringWithOffset = '2018-10-01T01:00:00-08:00'
